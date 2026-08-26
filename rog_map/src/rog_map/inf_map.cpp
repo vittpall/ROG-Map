4cc5c8c66f9ed66eebac39ec7a309bf83971751d
vittpall <vittorio.palladino@mail.polimi.it>
2026-08-25 18:01:27 -0700

    fixed zero velocity trajectory

--- rog_map/src/rog_map/inf_map.cpp
+++ rog_map/src/rog_map/inf_map.cpp
@@ -26,14 +26,14 @@
 namespace rog_map {
 // Public Query Function ========================================================================
     bool InfMap::isOccupiedInflate(const Vec3f &pos) const {
-        if (!insideLocalMap(pos)) return false;
+        if (!insideLocalMap(pos)) return true;
         if (pos.z() > cfg_.virtual_ceil_height) return true;
         if (pos.z() < cfg_.virtual_ground_height) return true;
         return imd_.occ_inflate_cnt[getHashIndexFromPos(pos)] > 0;
     }
 
     bool InfMap::isOccupiedInflate(const Vec3i &id_g) const {
-        if (!insideLocalMap(id_g)) return false;
+        if (!insideLocalMap(id_g)) return true;
         if (id_g.z() > cfg_.virtual_ceil_height_id_g) return true;
         if (id_g.z() < cfg_.virtual_ground_height_id_g) return true;
         return imd_.occ_inflate_cnt[getHashIndexFromGlobalIndex(id_g)] > 0;
